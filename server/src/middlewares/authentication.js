9275ee0f32f24f41e3f4caffbf705d1ae095e644
Vamsi <bvamsi489@gmail.com>
2024-03-28 12:27:51 +0530

    done

--- server/src/middlewares/authentication.js
+++ server/src/middlewares/authentication.js
@@ -3,18 +3,20 @@ require("dotenv").config();
 
 const authentication = (req, res, next) => {
     if(!req.headers.authorization) {
-        return res.json({message:"Please Login"});
+        return res.json({message:"Authorization failed, Please Login"});
     }
-    const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.JSON_KEY, function(err, decoded) {
-        if(err) {
-            res.json({message:"Please Login"})
-        }
-        else {
-            req.body.userId = decoded.userId;
-            next();
-        }
-    })
+    else {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.JSON_KEY, function(err, decoded) {
+            if(err) {
+                res.json({message:"Please Login"})
+            }
+            else {
+                req.body.userId = decoded.userId;
+                next();
+            }
+        })
+    }
 }
 
 module.exports = {authentication};
