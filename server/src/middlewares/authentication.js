04b270b2d52c30aff9656614c8d19ae91f5dca96
Vamsi <bvamsi489@gmail.com>
2024-03-28 12:21:24 +0530

    json error rectified

--- server/src/middlewares/authentication.js
+++ server/src/middlewares/authentication.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 
 const authentication = (req, res, next) => {
     if(!req.headers.authorization) {
-        return res.send("Please Login");
+        return res.json({message:"Please Login"});
     }
     const token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, process.env.JSON_KEY, function(err, decoded) {
         if(err) {
-            res.send("Please Login" + err)
+            res.json({message:"Please Login"})
         }
         else {
             req.body.userId = decoded.userId;
