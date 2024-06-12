b1c43688a91a030365db672af73094747be5152c
Vamsi-Shetty <bvamsi489@gmail.com>
2024-06-13 01:07:46 +0530

    update:changes done to ui

--- client/src/Pages/PageNotFound.jsx
+++ client/src/Pages/PageNotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className='text-center'>
         <h1>Oops! The page you are looking is missing.</h1>
         <p>Goto {" "} 
-            <Link to='/'>Homepage</Link></p>
+            <Link className='underline font-bold' to='/'>Homepage</Link></p>
     </div>
   )
 }
